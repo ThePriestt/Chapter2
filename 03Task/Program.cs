ea768f0d56e338d649606e1214533f0d0ad10277
ThePriestt <49431616+ThePriestt@users.noreply.github.com>
2020-10-13 23:40:48 +0300

    Update Program.cs

--- 03Task/Program.cs
+++ 03Task/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 // Напишете програма, която изчислява вярно променливи с плаваща запетая с точност до 0.000001.
 
@@ -8,7 +8,17 @@ class Program
     {
         decimal firstNumber = 10m;
         decimal secondNumber = 3m;
+
+        double fourthNumber = 10d;
+        double fifthNumber = 3d;
+
+        float sixthNumber = 10f;
+        float seventhNumber = 3f;
+
         Console.WriteLine(firstNumber / secondNumber);
+        Console.WriteLine(fourthNumber / fifthNumber);
+        Console.WriteLine(sixthNumber / seventhNumber);
+        
         // 3.3333333333333333333333333333 - decimal
         // 3.33333333333333 - double
         // 3.333333 - float
